c7730a52ce6072f47b2716a8a12d5262eaf9da77
Lukashmoser <73304107+Lukashmoser@users.noreply.github.com>
2022-01-13 11:23:20 -0800

    most recent version

--- CoinTrail/public/index-script.js
+++ CoinTrail/public/index-script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
--- CoinTrail/public/user-script.js
+++ CoinTrail/public/user-script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,11 +19,13 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // global variables
-let user = null;
-let displayname = null;
 const userList = firebase.database().ref('userList');
-let users = null;
-let uid = null;
+let user = null; // current users profile data
+let displayname = null; // tmp location for new users displayname
+let users = null; // list of users
+let uid = null; // tmp location for user id
+let userIndex = null; // location of current users profile in database
+let corsEnabled = false;
 
 window.onload = loadUser();
 
@@ -45,35 +51,105 @@ function loadUser(){
         if(users[i].uid == uid){
           userFound = true;
           user = users[i];
+          userIndex = i;
         }
       }
       // if doesn't exist create blank profile and add them to database
       if(!userFound){
-        users[users.length] = {
+        user = {
           uid: uid,
           username: displayname,
           coins: "none"
-        }
+        };
+        userIndex = users.length;
+        users[users.length] = user;
         userList.set(users);
       }
-
     }
-  });
+    // load welcome message
+    document.getElementById("welcome").innerHTML = "Welcome " + user.username;
+  }); // once
 } // loadUser
-/*function testFetch(){
+
+function search(elem){
+  let coin = document.getElementById(elem).value;
+  coin = coin.toLowerCase();
+  console.log("Lowercase " + coin);
+  coin = coin.replace(" ","");
+  let url = "https://cors-anywhere.herokuapp.com/api.coincap.io/v2/assets/" + coin;
+  console.log(url);
   var requestOptions = {
     method: 'GET',
     redirect: 'follow',
-    Authorization: 'Bearer 29d41298-b14f-4f99-be93-7c164af613a7'
+    header: {
+      'Authorization': 'Bearer 29d41298-b14f-4f99-be93-7c164af613a7',
+      'Accept-Encoding': 'gzip'
+    }
   };
+  if(corsEnabled){
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(result => loadCoin(result));
+  } else {
+    document.getElementById("message").innerHTML = "This feature requires cors to be enabled. Please visit the link <a onclick='toggleCORS()' href='https://cors-anywhere.herokuapp.com/corsdemo' target='_blank' rel='noopener noreferrer'>here</a> and click the request temporary access button.";
+    show("lightbox2");
+  }
   
-  fetch("https://api.coincap.io/v2/assets/bitcoin", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+}
 
-    fetch("https://api.coincap.io/v2/assets/bitcoin/history?interval=d1", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}*/
+function errorCheck(error, elem){
+  document.getElementById(elem).value = "";
+  let errorMessage = "That isn't the name of a coin. Please check your spelling and try again.";
+  document.getElementById("message").innerHTML = errorMessage;
+  show('lightbox2');
+}
+
+function loadCoin(coin){
+  if(coin.error != undefined){
+    console.log(coin.error);
+    let errorMessage = "That isn't the name of a coin. Please check your spelling and try again.";
+    document.getElementById("message").innerHTML = errorMessage;
+    show('lightbox2');
+  } else {
+    console.log(coin);
+    document.getElementById("coin-id").innerHTML = coin.data.name;
+    document.getElementById("current-price").innerHTML = coin.data.priceUsd;
+    loadChart(coin.data.id, 'm30');
+  }
+}
+
+function loadChart(id, interval){
+  let baseURL = "https://cors-anywhere.herokuapp.com/api.coincap.io/v2/assets/";
+  let url = "";
+  let timeStart = "";
+  let timeEnd = Date.now();
+  switch(interval) {
+    case "m30":
+      //case for 1 day
+      timeStart = timeEnd - 86400000;
+      break;
+    case "h2":
+      //case for 5 days
+      timeStart = timeEnd - 432000000;
+      break;
+    case "h12":
+      //case for 1 month
+      timeStart = timeEnd - 2629800000;
+      break;
+  }
+  url = baseURL + id + "/history?interval=" + interval + "&start=" + timeStart + "&end=" + timeEnd;
+  console.log(url);
+
+}
+
+function toggleCORS(){
+  corsEnabled = true;
+}
+
+function hide(elem){
+  document.getElementById(elem).style.display = "none";
+} // hide
+
+function show(elem){
+  document.getElementById(elem).style.display = "block";
+} // show
