70c03f7bf945c64935bd364b236721294ef9755d
Lukashmoser <73304107+Lukashmoser@users.noreply.github.com>
2022-01-21 09:09:55 -0800

    removed firebase info

--- public/index-script.js
+++ public/index-script.js
@@ -6,14 +6,7 @@ if ('serviceWorker' in navigator) {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-A08SvLXIL8YSZoglfySwUDesesEPMIs",
-  authDomain: "coin-trail.firebaseapp.com",
-  databaseURL: "https://coin-trail-default-rtdb.firebaseio.com",
-  projectId: "coin-trail",
-  storageBucket: "coin-trail.appspot.com",
-  messagingSenderId: "981030871637",
-  appId: "1:981030871637:web:b0004637e68cd347fd6adc",
-  measurementId: "G-25E4SL971W"
+  // your firebase config goes here
 };
 
 // firebase inits
